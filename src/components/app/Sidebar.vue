40921ac253806354398c608ab41e86e3492aae9c
MRXPRO200330020179Y <mrxpro2003@gmail.com>
2022-12-16 14:48:51 +0500

    Templates-Vue-Front

---
+++ src/components/app/Sidebar.vue
@@ -0,0 +1,55 @@
+<template>
+    <ul class="sidenav app-sidenav" :class="{open: value}">
+
+        <li v-for="(iink,index) in links" :key="index + iink.url">
+            <router-link active-class="active" exact :to="iink.url" class="waves-effect waves-orange pointer">
+                {{iink.title}}
+            </router-link>
+        </li>
+
+    </ul>
+</template>
+
+<style lang="scss">
+    .sidenav{
+        li{
+            .active{
+                background-color: #f2f2f2;
+            }
+        }
+    }
+</style>
+
+<script>
+export default {
+    props:['value'],
+    data: () =>({
+        links:[
+            {
+                title:'Счет',
+                url:'/'
+            },
+
+            {
+                title:'История',
+                url:'/history'
+            },
+
+            {
+                title:'Планирование',
+                url:'/planning'
+            },
+
+            {
+                title:'Новая запись',
+                url:'/record'
+            },
+
+            {
+                title:'Категории',
+                url:'/categories'
+            }
+        ]
+    })
+}
+</script>
---
+++ src/layouts/MainLayout.vue
@@ -0,0 +1,36 @@
+<template>
+    <div class="app-main-layout">
+      
+      <NavbarVue @IsOpenClik="isOpen = !isOpen"></NavbarVue>
+      <SidebarVue :value="isOpen"></SidebarVue>
+  
+      <main class="app-content" :class="{full:!isOpen}">
+        <div class="app-page">
+          <router-view />
+        </div>
+      </main>
+  
+      <div class="fixed-action-btn">
+        <a class="btn-floating btn-large" href="#">
+          <i class="large material-icons">add</i>
+        </a>
+      </div>
+    </div>
+</template>  
+
+<script>
+import NavbarVue from '@/components/app/Navbar.vue'
+import SidebarVue from '@/components/app/Sidebar.vue'
+export default {
+    name: 'main-layout',
+
+    data: () => ({
+        isOpen:true
+    }),
+
+    components:{
+        NavbarVue,
+        SidebarVue
+    },
+}
+</script>
